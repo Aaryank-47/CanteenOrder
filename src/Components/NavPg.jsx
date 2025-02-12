82ffa3cdef641e366ed486d2c00d4d0783d0e4fb
Aaryan Kamalwanshi <aaryankamalwanshi274@gmail.com>
2025-02-12 20:49:48 +0530

    proper fixing mobileview

--- src/Components/NavPg.jsx
+++ src/Components/NavPg.jsx
@@ -1,88 +1,41 @@
-import "./NavCSS.css";
-import { useState, useEffect } from "react";
+import "./NavCSS.css"
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import About from "./NavContent/About";
-import Menu from "./NavContent/Menu";
-import Info from "./NavContent/info";
-import Orderhistory from "./NavContent/OrderHistory";
+import About from "./NavContent/About"
+import Menu from "./NavContent/Menu"
+import Info from "./NavContent/info"
+import Orderhistory from "./NavContent/OrderHistory"
 import Home from "./Home";
 import LoginPg from "./LoginPg";
-import "./R_App.css";
+import "./R_App.css"
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavPg = () => {
+
     const [isLoginOpen, setLoginOpen] = useState(false);
     const [isMobileView, setMobileView] = useState(false);
-
-    // Ensure menu starts closed & handle screen resizing
-    useEffect(() => {
-        // setMobileView(false); 
-        // Close menu when component mounts
-
-        const handleResize = () => {
-            if (window.innerWidth > 715) {
-                setMobileView(false); // Close menu when switching to desktop
-            }
-            
-        };
-
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
 
     return (
         <>
             <div>
-                <Router>
+                <Router >
                     <div className="nav">
-                        <h1 className="logo">RESTRO</h1>
-
-                        {/* Hamburger Menu */}
-                        <div className="hamburger" onClick={() => setMobileView(isMobileView)}>
-                            {isMobileView ? <FaTimes /> : <FaBars />}
+                        <h1 className=".logo">RESTRO</h1>
+                        <div className="hamburger" onClick={() => setMobileView(!isMobileView)}>
+                            
+                            {isMobileView ? <FaBars /> : <FaTimes />}
                         </div>
-
-                        {/* Navigation List */}
                         <ul className={`nav-list ${isMobileView ? "active" : ""}`}>
-                            <li className="nav-item">
-                                <Link to="/" onClick={() => setMobileView(true)}>
-                                    Home
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/about" onClick={() => setMobileView(true)}>
-                                    About US
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/menu" onClick={() => setMobileView(true)}>
-                                    Menu
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/info" onClick={() => setMobileView(true)}>
-                                    Info
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/orderhistory" onClick={() => setMobileView(true)}>
-                                    Order History
-                                </Link>
-                            </li>
-                            <button className="nav-item" id="login" onClick={() => {
-                                setLoginOpen(true);
-                                setMobileView(true);
-                            }}>
-                                LOGIN
-                            </button>
+                            <li className="nav-item"><Link to="/" onClick={() => setMobileView(false)}>Home</Link></li>
+                            <li className="nav-item"><Link to="/about" onClick={() => setMobileView(false)}>About US</Link></li>
+                            <li className="nav-item"><Link to="/menu" onClick={() => setMobileView(false)}>Menu</Link></li>
+                            <li className="nav-item"><Link to="/info" onClick={() => setMobileView(false)}>Info</Link></li>
+                            <li className="nav-item"><Link to="/orderhistory" onClick={() => setMobileView(false)}>Order History</Link></li>
+                            <button className="nav-item" id="login" onClick={() => { setLoginOpen(true); setMobileView(false); }}>LOGIN</button>
                         </ul>
                     </div>
-
-                    <hr />
-                    <hr />
-
-                    {/* Page Routing */}
+                    <hr /><hr />
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/about" element={<About />} />
@@ -90,13 +43,13 @@ const NavPg = () => {
                         <Route path="/info" element={<Info />} />
                         <Route path="/orderhistory" element={<Orderhistory />} />
                     </Routes>
-
-                    {/* Login Modal */}
                     {isLoginOpen && <LoginPg isOpen={isLoginOpen} onClose={() => setLoginOpen(false)} />}
                 </Router>
+
+
             </div>
         </>
-    );
-};
+    )
+}
 
-export default NavPg;
+export default NavPg
